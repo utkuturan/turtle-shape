147e53252849702ee369e4e424f03a1efd70e07e
utkuturan <utkuturan1994@gmail.com>
2024-08-18 17:09:00 +0300

    random_color added

--- random_walk.py
+++ random_walk.py
@@ -3,13 +3,21 @@ import random
 
 
 tim = t.Turtle()
+t.colormode(255)
 
-colours = ["aquamarine", "crimson","medium blue","chartreuse","purple","deep pink","light coral","yellow","light sky blue","dark green","dark slate blue"]
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r, g, b)
+    return random_color
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
